ee3d7725319d00ba5ee98dd0803df811bf78cd5a
Olamide Ifarajimi <lordelami@gmail.com>
2026-04-22 17:40:08 +0100

    New main page Handler Function added. To be tested

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ var templateFS fs.FS = os.DirFS("templates")
 
 var theTemplates *template.Template = template.Must(template.ParseFS(templateFS, "**/*.html"))
 
-var page int = 0
+var pageNum int = 0
 
-var Page struct {
+var page struct {
 	Title   string
 	Content template.HTML
 }
@@ -36,12 +36,22 @@ type artistDetail struct {
 	Relations    string
 }
 
-func getArtistsPage(w http.ResponseWriter, r *http.Request) {
+type pageLink struct {
+	Name string
+	Url  string
+}
+
+type object struct {
+	Name     string
+	Elements []string
+}
+
+func getArtistspage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Method, r.URL.Path)
-	nbrOfItemsInPage := 6
-	artistsDetails := make([]artistDetail, nbrOfItemsInPage)
+	nbrOfItemsInpage := 6
+	artistsDetails := make([]artistDetail, nbrOfItemsInpage)
 	for i := range 6 {
-		id := strconv.Itoa(page*nbrOfItemsInPage + (i + 1))
+		id := strconv.Itoa(pageNum*nbrOfItemsInpage + (i + 1))
 		artistLink := "https://groupietrackers.herokuapp.com/api/artists/" + id
 		resp, err := http.Get(artistLink)
 		if err != nil {
@@ -69,16 +79,70 @@ func getArtistsPage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	Page.Title = "Artists"
-	Page.Content = template.HTML(content.String())
-	err = theTemplates.ExecuteTemplate(w, "layout.html", Page)
+	page.Title = "Artists"
+	page.Content = template.HTML(content.String())
+	err = theTemplates.ExecuteTemplate(w, "layout.html", page)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func groupieTrackerApiResponseBody(path string) (body []byte) {
+	apiLink := "https://groupietrackers.herokuapp.com/api" + path
+	resp, err := http.Get(apiLink)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func hanleMainpage(w http.ResponseWriter, r *http.Request) {
+	// Get the response body from the API
+	var apiPathMainData map[string]string
+	err := json.Unmarshal(groupieTrackerApiResponseBody(""), &apiPathMainData)
+	if err != nil {
+		panic(err)
+	}
+
+	// Render the links from the API as html anchor tags
+	links := []string{}
+	for name, url := range apiPathMainData {
+		var linkHtml strings.Builder
+		err := theTemplates.ExecuteTemplate(&linkHtml,
+			"pageurlstring.html",
+			pageLink{name, url})
+		if err != nil {
+			panic(err)
+		}
+		links = append(links, linkHtml.String())
+	}
+
+	// Render the html links as part of the index page object div
+	var indexPageDiv strings.Builder
+	err = theTemplates.ExecuteTemplate(&indexPageDiv,
+		"object.html",
+		object{"main-page", links})
+	if err != nil {
+		panic(err)
+	}
+
+	// Render the main page on the browser
+	page.Title = "Index"
+	page.Content = template.HTML(indexPageDiv.String())
+	err = theTemplates.ExecuteTemplate(w, "layout.html", page)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	http.HandleFunc("GET /", getArtistsPage)
+	http.HandleFunc("GET /{$}", hanleMainpage)
 
 	log.Panicln(http.ListenAndServe(":8080", nil))
 }
